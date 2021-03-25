a9ebcb81fecf81d20c4d2c0c58bc87618b2f72db
Rana Alqrenawi <rana@gazaskygeeks.com>
2021-03-25 15:56:32 +0000

    update get walks to get the date in a format of year, month and day

--- LambdaCSharpWebAPI/Models/WalkAvgRatingModel.cs
+++ LambdaCSharpWebAPI/Models/WalkAvgRatingModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace LambdaCSharpWebAPI.Models
 {
@@ -6,6 +7,10 @@ namespace LambdaCSharpWebAPI.Models
     {
         public string Id { get; set; }
         public string WalkName { get; set; }
+        public string Walktime { get; set; }
+        public string YearOf { get; set; }
+        public string MonthOf { get; set; }
+        public string DayOf { get; set; }
         public string UserID { get; set; }
         public float AveRating { get; set; }
     }
